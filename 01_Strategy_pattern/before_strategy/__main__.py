7546eb3f86abeaa49371e5146f383f67f0628999
dev2 <c.chaiwat.ch@gmail.com>
2021-04-08 12:33:57 +0700

    update: more test case
    Signed-off-by: dev2 <c.chaiwat.ch@gmail.com>

--- 01_Strategy_pattern/before_strategy/__main__.py
+++ 01_Strategy_pattern/before_strategy/__main__.py
@@ -6,3 +6,13 @@ order = Order(Shipper.fedex)
 cost_calulator = ShippingCost()
 cost = cost_calulator.shipping_cost(order)
 assert cost == 3.0
+
+order = Order(Shipper.ups)
+cost_calulator = ShippingCost()
+cost = cost_calulator.shipping_cost(order)
+assert cost == 4.0
+
+order = Order(Shipper.postal)
+cost_calulator = ShippingCost()
+cost = cost_calulator.shipping_cost(order)
+assert cost == 5.0
